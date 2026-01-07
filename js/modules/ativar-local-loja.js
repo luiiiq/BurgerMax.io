bb8a0e52748b85c1dd7f4179e0753b2a72bae783
Luiza <luizabenevides2007@gmail.com>
2026-01-07 15:00:14 -0300

    Adicionado package-lock

--- js/modules/ativar-local-loja.js
+++ js/modules/ativar-local-loja.js
@@ -1,7 +1,6 @@
 export default function localLoja(aside, deletar) {
   const asideElemento = document.querySelector(aside);
   const deletarElemento = document.querySelector(deletar);
-
   if (asideElemento && deletarElemento) {
     function desativarElemento() {
       asideElemento.classList.add("desativado");
