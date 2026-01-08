9bc6a1f158e089e1dd402bf4d7917ee4da1e1b9f
Luiza <luizabenevides2007@gmail.com>
2026-01-08 16:46:06 -0300

    Anima-scroll com class

--- js/modules/scroll-sections.js
+++ js/modules/scroll-sections.js
@@ -1,19 +1,29 @@
-export default function animaScroll(sections) {
-  const sectionsLista = document.querySelectorAll(sections);
+export default class AnimaScroll {
+  constructor(sections) {
+    this.sectionsLista = document.querySelectorAll(sections);
+    this.animaSecao = this.animaSecao.bind(this);
+  };
 
-  if (sectionsLista.length) {
-    function animaSecao() {
-      sectionsLista.forEach((section) => {
-        const windowMetade = window.innerHeight * 0.6;
-        const sectionTopo = section.getBoundingClientRect().top;
-        const sectionVisivel = sectionTopo - windowMetade;
-        if (sectionVisivel < 0) {
-          section.classList.add("ativar-scroll");
-        }
-      });
-    }
-    animaSecao();
+  animaSecao() {
+    this.sectionsLista.forEach((section) => {
+      const windowMetade = window.innerHeight * 0.6;
+      const sectionTopo = section.getBoundingClientRect().top;
+      const sectionVisivel = sectionTopo - windowMetade;
+      if (sectionVisivel < 0) {
+        section.classList.add("ativar-scroll");
+      };
+    });
+  };
 
-    window.addEventListener("scroll", animaSecao);
-  }
-}
+  adicionarScrollEvent() {
+    window.addEventListener("scroll", this.animaSecao);
+  };
+
+  init() {
+    if (this.sectionsLista.length) {
+      this.animaSecao();
+      this.adicionarScrollEvent();
+    };
+    return this;
+  };
+};
