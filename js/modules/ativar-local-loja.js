46866d6d91fcc596008b000396a6945893728723
Luiza <luizabenevides2007@gmail.com>
2026-01-08 15:00:54 -0300

    Local da loja ajustado

--- js/modules/ativar-local-loja.js
+++ js/modules/ativar-local-loja.js
@@ -1,11 +1,22 @@
-export default function localLoja(aside, deletar) {
-  const asideElemento = document.querySelector(aside);
-  const deletarElemento = document.querySelector(deletar);
-  if (asideElemento && deletarElemento) {
-    function desativarElemento() {
-      asideElemento.classList.add("desativado");
-    }
+export default class LocalLoja {
+  constructor(aside, deletar) {
+    this.asideElemento = document.querySelector(aside);
+    this.deletarElemento = document.querySelector(deletar);
+    this.desativarElemento = this.desativarElemento.bind(this);
+  };
 
-    deletarElemento.addEventListener("click", desativarElemento);
-  }
-}
+  desativarElemento() {
+    this.asideElemento.classList.add("desativado");
+  };
+
+  adicionarDesativarEvent() {
+    this.deletarElemento.addEventListener("click", this.desativarElemento);
+  };
+
+  init() {
+    if (this.asideElemento && this.deletarElemento) {
+      this.adicionarDesativarEvent();
+    };
+    return this;
+  };
+};
